abc185624af0e3503ff9359c5e512bc33a864b6c
flavionanni <flaviogermanoperez@gmail.com>
2021-09-30 10:04:41 -0300

    Criando arquivo do jogo da forca

---
+++ forca/forca.py
@@ -0,0 +1,8 @@
+
+def jogar():
+
+    print("********************************")
+    print("***Bem vindo no jogo da Forca***")
+    print("********************************")
+
+    print("Fim do Jogo")
