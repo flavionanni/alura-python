80c2d21dc044991e5b3b3e5dbafba4695fa05c76
flavionanni <flaviogermanoperez@gmail.com>
2021-09-29 22:34:04 -0300

    Implementação da dificuldade e pontuação do jogo

--- adivinhacao/adivinhacao.py
+++ adivinhacao/adivinhacao.py
@@ -4,8 +4,25 @@ print("********************************")
 print("Bem vindo no jodo de Adivinhação")
 print("********************************")
 
+print("*"* 10, "Nível de Dificuldade", "*" * 10)
+print("(1) Fácil", "(2) Médio", "(3) Difícil", "*" * 42, sep="\n")
+
+pontuacao = 1000
+
+
+dificuldade = int(input("Digitel o nível: "))
+
+if(dificuldade == 1):
+    total_de_tentativas = 20
+
+elif(dificuldade == 2):
+    total_de_tentativas = 10
+
+else:
+    total_de_tentativas = 5
+
 numero_secreto = random.randrange(1,101)
-total_de_tentativas = 5
+
 
 for rodada in range(1, total_de_tentativas + 1):
     print(f"Tentativa {rodada:02d} de {total_de_tentativas:02d}")
@@ -22,7 +39,7 @@ for rodada in range(1, total_de_tentativas + 1):
     menor = chute_usuario < numero_secreto
 
     if(acertou):
-        print("Você acertou!")
+        print(f"Você acertou! e obteve um total de {pontuacao} pontos.")
         break
 
     else:
@@ -31,5 +48,10 @@ for rodada in range(1, total_de_tentativas + 1):
 
         elif(menor):
             print("Você errou! O seu chute foi menor que o número secreto.")
+
+    
+    diferenca_pontuacao = abs(numero_secreto - chute_usuario)
+    pontuacao = pontuacao - diferenca_pontuacao
+
 
 print(f"Fim do jogo, o número secréto era o {numero_secreto}!")
