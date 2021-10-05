31103411e3c94a0b38e8278e7fd37cd114e52641
flavionanni <flaviogermanoperez@gmail.com>
2021-10-05 16:59:52 -0300

    Encapsulamento da classe Conta

--- poo/banco/conta.py
+++ poo/banco/conta.py
@@ -2,18 +2,37 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
         print("Criando um objeto... {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def verificar_saldo(self):
-        print(f"Saldo: R${self.saldo:.{2}f}")
+        print(f"Saldo: R${self.__saldo:.{2}f}")
 
     def depositar(self, valor):
-        self.saldo += valor
-        self.extrato()
+        self.__saldo += valor
 
     def sacar(self, valor):
-        self.saldo -= valor
-        self.extrato()
+        self.__saldo -= valor
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
+
+    # Gets and Setters
+
+    def get_numero(self):
+        return self.__numero
+
+    def get_titular(self):
+        return self.__titular
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_limite(self):
+        return self.__limite
+
+    def setter_(self, limite):
+        self.__limite = limite
